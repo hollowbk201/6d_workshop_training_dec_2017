497b50f8f3589321e18de3ccf9cfb0defd93be8f
hollowbk201 <heihollowbk201@gmail.com>
2017-12-19 02:07:35 -0600

    Mas cambios a codigo prueba

--- src/test/java/com/sixdelta/MultiplosTest.java
+++ src/test/java/com/sixdelta/MultiplosTest.java
@@ -9,6 +9,7 @@ public class MultiplosTest {
 	public void firstTest(){
 		Multiplos m = new Multiplos();
 		int result = m.calculateFor(10);
+		System.out.println("valor para 1000 : " + m.calculateFor(1000));
 		assertEquals(23,result);
 	}
 }
